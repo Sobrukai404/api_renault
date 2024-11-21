a7a3efb5f575619d9df145351412d7d88666b0bc
Sobrukai404 <lucas.lima4138@gmail.com>
2024-11-21 01:03:19 -0300

    working, but it needs the modification endpoints

--- src/main/java/com/api/renault/ApiRenaultApplication.java
+++ src/main/java/com/api/renault/ApiRenaultApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiRenaultApplication {
+public class  ApiRenaultApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRenaultApplication.class, args);
 	}
